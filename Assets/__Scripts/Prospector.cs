39ab7a2ebe9125666ad0206d18dfd609501a5f56
Nick Edwards <nicholas6598@gmail.com>
2020-11-19 16:08:22 -0500

    updated prospector

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,10 +11,18 @@ public class Prospector : MonoBehaviour
     [Header("Set in Inspector")]
     public TextAsset deckXML;
     public TextAsset layoutXML;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Vector3 layoutCenter;
 
     [Header("Set Dynamically")]
     public Deck deck;
     public Layout layout;
+    public List <CardProspector> drawPile;
+    public Transform layoutAnchor;
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
 
     void Awake(){
         S = this;
@@ -35,5 +43,17 @@ public class Prospector : MonoBehaviour
 
         layout = GetComponent<Layout>();
         layout.ReadLayout(layoutXML.text);
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD){
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach(Card tCD in lCD){
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
     }
 }
